87b93c9d59751fa0d58fb28feee6de3e94a77bf6
siddesh bagare <siddeshbagare@gmail.com>
2021-12-02 12:44:16 +0530

    Add project files.

---
+++ IfElseSwitch/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace IfElseSwitch
+{
+    class Marks
+    {
+        public int Physics { get; set; }
+        public int Chemistry { get; set; }
+        public int Maths { get; set; }
+
+        public void Result()
+        {
+            Console.WriteLine("");
+        }
+    }
+    class Student
+    {
+        public string Name;
+        public int Rno;
+
+        public void Display()
+        {
+            Console.WriteLine("List of all Students :");
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("\tRoll No : {0}", i + 1);
+                Console.Write("\t\tName : ");
+
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("How many Students");
+            int num =Int32.Parse(Console.ReadLine());
+
+            Student[] Students = new Student[num];
+
+            Console.WriteLine("Enter the names of Students :");
+
+            for (int i=0;i<num;i++)
+            {
+                Console.WriteLine("\tRoll No : {0}",i+1);
+                Console.Write("\t\tName : ");
+
+                Students[i].Name=Console.ReadLine();
+                Students[i].Rno= i+1;
+            }
+
+        }
+    }
+}
